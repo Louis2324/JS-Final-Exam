fa801ee40f2bf76f75e390a3e367c8b946f5dc71
Louis2324 <miguellouis2023@gmail.com>
2025-06-16 16:56:06 +0200

    Added proper rrequest check

--- middleware/customParser.js
+++ middleware/customParser.js
@@ -1,7 +1,20 @@
 const parser = (req, res, next) => {
+    if (!["POST", "PUT", "PATCH"].includes(req.method)) {
+        return next();
+    }
+
     let rawData = '';
-    req.on('data', chunk => rawData += chunk);
+
+    req.on('data', chunk => {
+        rawData += chunk;
+    });
+
     req.on('end', () => {
+        if (!rawData) {
+            req.body = {};
+            return next();
+        }
+
         try {
             req.body = JSON.parse(rawData);
             next();
@@ -9,5 +22,11 @@ const parser = (req, res, next) => {
             res.status(400).json({ error: "Invalid JSON data" });
         }
     });
+
+    req.on('error', (err) => {
+        console.error("Request error:", err);
+        res.status(400).json({ error: "Error reading request body" });
+    });
 };
-module.exports = {parser};
+
+module.exports = { parser };
