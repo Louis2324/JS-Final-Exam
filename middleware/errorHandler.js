4216243a41f58ae24196124feacbe2f5e22a53b6
Louis2324 <miguellouis2023@gmail.com>
2025-06-16 17:11:05 +0200

    added modules export to the end of the file

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -2,3 +2,5 @@ const errorHandler  = (err, req, res, next) => {
   console.error("Error:", err.message);
   res.status(500).json({ error: "Internal Server Error" });
 }
+
+module.exports = { errorHandler }
